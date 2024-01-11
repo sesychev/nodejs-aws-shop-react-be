df6a44baffa4dd0cd9d5c74899c70516e18ce9b5
sesychev <sergeysychou@gmail.com>
2024-01-11 03:41:34 +0300

    task-10: add bff-service

--- bff-service/src/app.module.ts
+++ bff-service/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({}),
+    HttpModule,
+    CacheModule.register({ ttl: 120 }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
